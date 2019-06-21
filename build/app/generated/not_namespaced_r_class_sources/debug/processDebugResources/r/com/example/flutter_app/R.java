6ea3792eb35fdaad81c007bbf6beb26bb9d65e4b
Shakeane Hinds <SHinds@realdecoy.com>
2019-06-21 18:53:17 -0500

    updated splash image and logo for android

--- build/app/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/example/flutter_app/R.java
+++ build/app/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/example/flutter_app/R.java
@@ -14,7 +14,10 @@ public final class R {
   public static final class mipmap {
     public static final int ic_launcher=0x7f020000;
     public static final int logo=0x7f020001;
-    public static final int splash=0x7f020002;
+    public static final int logopng=0x7f020002;
+    public static final int logotext=0x7f020003;
+    public static final int moon=0x7f020004;
+    public static final int splash=0x7f020005;
   }
   public static final class style {
     public static final int LaunchTheme=0x7f030000;
